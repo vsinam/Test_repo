3aac68d5215e9cf059f9a693fb8453615dd08249
vikas <vikassinam@gmail.com>
2021-01-17 20:41:41 +0530

    modified in branch

--- sample.py
+++ sample.py
@@ -1,6 +1,8 @@
 print("hello this is my sample file")
-age = input("enter your age")
-if age >= 18:
-    print("you are adult")
+age = int(input("enter your age"))
+if age >= 60:
+    print("you are senier citizen")
+elif age >=18:
+    print("you are a adult")
 else:
-    print("you are a kid")
+    print("You are a kid")
